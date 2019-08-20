dbdf55538726c388911e71e382dac380c91361a3
Harkiran Grewal <40836336+harkirangrewal@users.noreply.github.com>
2019-08-20 15:37:48 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Connect_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb_grid = new System.Windows.Forms.PictureBox();
+            this.txtYellow = new System.Windows.Forms.TextBox();
+            this.lblYellow = new System.Windows.Forms.Label();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.txtRed = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_grid)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pb_grid
+            // 
+            this.pb_grid.Location = new System.Drawing.Point(30, 47);
+            this.pb_grid.Name = "pb_grid";
+            this.pb_grid.Size = new System.Drawing.Size(700, 600);
+            this.pb_grid.TabIndex = 0;
+            this.pb_grid.TabStop = false;
+            this.pb_grid.Paint += new System.Windows.Forms.PaintEventHandler(this.pb_grid_Paint);
+            // 
+            // txtYellow
+            // 
+            this.txtYellow.Location = new System.Drawing.Point(146, 38);
+            this.txtYellow.Name = "txtYellow";
+            this.txtYellow.Size = new System.Drawing.Size(100, 26);
+            this.txtYellow.TabIndex = 1;
+            // 
+            // lblYellow
+            // 
+            this.lblYellow.AutoSize = true;
+            this.lblYellow.Location = new System.Drawing.Point(34, 38);
+            this.lblYellow.Name = "lblYellow";
+            this.lblYellow.Size = new System.Drawing.Size(55, 20);
+            this.lblYellow.TabIndex = 2;
+            this.lblYellow.Text = "Yellow";
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Location = new System.Drawing.Point(34, 101);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(39, 20);
+            this.lblRed.TabIndex = 3;
+            this.lblRed.Text = "Red";
+            // 
+            // txtRed
+            // 
+            this.txtRed.Location = new System.Drawing.Point(146, 95);
+            this.txtRed.Name = "txtRed";
+            this.txtRed.Size = new System.Drawing.Size(100, 26);
+            this.txtRed.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtRed);
+            this.groupBox1.Controls.Add(this.lblRed);
+            this.groupBox1.Controls.Add(this.lblYellow);
+            this.groupBox1.Controls.Add(this.txtYellow);
+            this.groupBox1.Location = new System.Drawing.Point(781, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 191);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1104, 672);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pb_grid);
+            this.Name = "Form1";
+            this.Text = "Connect Four";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_grid)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_grid;
+        private System.Windows.Forms.TextBox txtYellow;
+        private System.Windows.Forms.Label lblYellow;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.TextBox txtRed;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Method "new game" makes everything equal to -1
+
+
+        /*private int Search_Array()
+        int rows;
+        {
+          for (rows = 6; rows < 7; rows= rows - 1)
+        } 
+        return 
+        */
+
+            /* for (row =5; row <=5; r--)
+            {
+                  if(ConnectBoard[col,row] == -1)
+                  {
+                  return row;
+                  }
+                else
+            }
+        
+
+
+        int[,] ConnectBoard = new int[7, 6];
+        // -1 empty
+        // 0 Yellow
+        // 1 Red
+
+        private void pb_grid_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            Pen black_pen = new Pen(Color.Black, 5);
+
+            g.DrawLine(black_pen, 0, 0, 700, 0);
+            g.DrawLine(black_pen, 0, 0, 0, 600);
+            g.DrawLine(black_pen, 0, 600, 700, 600);
+            g.DrawLine(black_pen, 700, 600, 700, 0);
+            // The outline of the connect four grid
+
+            g.DrawLine(black_pen, 0, 100, 700, 100);
+            g.DrawLine(black_pen, 0, 200, 700, 200);
+            g.DrawLine(black_pen, 0, 300, 700, 300);
+            g.DrawLine(black_pen, 0, 400, 700, 400);
+            g.DrawLine(black_pen, 0, 500, 700, 500);
+            g.DrawLine(black_pen, 0, 600, 700, 600);
+            // The outline of the inner grid 
+
+            g.DrawLine(black_pen, 100, 0, 100, 600);
+            g.DrawLine(black_pen, 200, 0, 200, 600);
+            g.DrawLine(black_pen, 300, 0, 300, 600);
+            g.DrawLine(black_pen, 400, 0, 400, 600);
+            g.DrawLine(black_pen, 500, 0, 500, 600);
+            g.DrawLine(black_pen, 600, 0, 600, 600);
+            g.DrawLine(black_pen, 700, 0, 700, 600);
+            // The outline of the veritcal lines
+
+        }
+    }
+}
